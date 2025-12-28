a02ba332d4919da6ced4ef3a829654fdc3b4499f
yordanos-berihun <yordanosberihun2@gmail.com>
2025-12-28 17:19:14 +0300

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,6 @@
-const User = require('./user');
-const Room = require('./room');
-const Message = require('./message');
+const {User} = require('./user');
+const {Room} = require('./room');
+const {Message} = require('./message');
 
 module.exports = {
   User,
